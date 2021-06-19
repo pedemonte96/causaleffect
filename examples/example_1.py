c76c8779201e6efba97376defd4a61684f68e5c5
pedemonte96 <pedemonte96@gmail.com>
2021-06-19 17:58:44 +0200

    fixed example and added documentation

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,9 +1,9 @@
-from causaleffect import *
+from causaleffect import createGraph, plotGraph, graph
 
 '''Code in Figure 3.5 (a)'''
 
 edges = ['X<->Z', 'X<->W', 'X->Z', 'Z->W', 'W->Y', 'X->Y']
 G = createGraph(edges)
-print(to_R_notation(edges))
+print(graph.to_R_notation(edges))
 
 plotGraph(G)
