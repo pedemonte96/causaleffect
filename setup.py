d112284ca5f34488d2a3dfcf239789998c09c3e8
Martí <32159046+pedemonte96@users.noreply.github.com>
2021-07-12 11:49:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '0.0.2'
+version = '0.0.3'
 description = 'Computing causal effects'
 long_description = """
 # causaleffect
@@ -39,6 +39,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
-        "Operating System :: Microsoft :: Windows",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ]
 )
